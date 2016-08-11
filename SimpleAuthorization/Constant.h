a623de309ba506d08df70d57f0121352b5ce85a9
Ryan Huynh <ryanbaohuynh@gmail.com>
2016-08-11 04:23:54 -0700

    sample code

---
+++ SimpleAuthorization/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  SimpleAuthorization
+//
+//  Created by Ryan Huynh on 8/10/16.
+//  Copyright © 2016 Infoway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constant : NSObject
+
+@end
---
+++ SimpleAuthorization/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  SimpleAuthorization
+//
+//  Created by Ryan Huynh on 8/10/16.
+//  Copyright © 2016 Infoway. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
